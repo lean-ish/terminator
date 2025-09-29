5326d39f6ea3c9d1da6d9fe54b93c6c7eada0543
Leandro Aguiar <leandro.aguiar@nosto.com>
2025-09-29 22:24:30 +0300

    adding spotless, which added file headers

--- src/main/java/io/github/leanish/terminator/Terminable.java
+++ src/main/java/io/github/leanish/terminator/Terminable.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2025 Leandro Aguiar
+ * Licensed under the MIT License.
+ * See LICENSE file in the project root for full license information.
+ */
 package io.github.leanish.terminator;
 
 /**
